62059cba783cf14f1b705f5e39d9a51ec8ecede5
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2023-11-28 22:15:03 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,51 @@
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+  idProduct INT,
+  nameProduct VARCHAR(100),
+  categoryProduct VARCHAR(100),
+  quantityProduct INT,
+  priceProduct FLOAT(10,2),
+  order_date DATE
+  
+);
+
+INSERT INTO products VALUES 
+	(1, "Television", "Eletronic", 1500, 5000.00, "2020-05-10"),
+    (2, "Toaster", "Eletronic", 2500, 15000.00, "2020-05-11"),
+    (3, "Notebook", "Eletronic", 3000, 3800.00, "2020-05-10"),
+    (4, "Paper Book", "Stationery", 15000, 10.00,"2020-05-12"), 
+    (5, "Digital Blackboard", "Stationery", 1000, 15000.50, "2020-05-11"),
+    (6, "Battery", "Stationery", 1000, 25.75, "2020-05-12"),
+    (7, "Controller", "Game", 1000, 55.75, "2020-05-11"),
+    (8, "God of War 5", "Game", 1500, 35.00, "2020-05-10"),
+    (9, "IPhone", "Eletronic", 1500, 1335.00, "2020-05-13"),
+    (10, "Game Chair", "Office", 1000, 800.00, "2020-05-10");
+
+
+SELECT order_date 
+ , SUM(CASE WHEN categoryProduct = "Eletronic" THEN priceProduct
+       ELSE "-"
+       END) AS "Eletronic Amount"
+ , SUM(CASE WHEN categoryProduct = "Stationery" THEN priceProduct
+       ELSE "-"
+       END) AS "Stationery Amount"
+ , SUM(CASE WHEN categoryProduct = "Game" THEN priceProduct
+       ELSE "-"
+       END) AS "Game Amount"
+ , SUM(CASE WHEN categoryProduct = "Office" THEN priceProduct
+       ELSE "-"
+       END) AS "Office Amount"
+ FROM products
+ GROUP BY 1
+ ;
+
+
+
+
+-------
+
+
+
 USE banco;
 -- DESCRIBE categorias; #df.info() do python
 DESCRIBE produtos;
